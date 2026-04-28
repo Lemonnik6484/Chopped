7a6f6b54e7e5b9a1d9098aa4044b803e624b8707
lemonnik <lemongamer6484@gmail.com>
2026-04-28 19:11:29 +0200

    init

---
+++ src/main/java/dev/lemonnik/chopped/Chopped.java
@@ -0,0 +1,42 @@
+package dev.lemonnik.chopped;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.resources.ResourceLocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//? if fabric
+//? if neoforge {
+/*import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+*/
+//?}
+
+//? if neoforge
+//@Mod(MOD_ID)
+
+@SuppressWarnings("removal")
+public class Chopped
+		//? if fabric
+		implements ModInitializer
+
+		{
+	public static final String MOD_ID = "chopped";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static final String VERSION = /*$ mod_version */"0.8.1";
+
+	//? if fabric
+	@Override public void onInitialize()
+
+	//? if neoforge
+	 //public Chopped(IEventBus modBus)
+
+	{
+		LOGGER.info("Starting Chopped...");
+	}
+
+	public static ResourceLocation id(String namespace, String path) {
+		return ResourceLocation.tryBuild(namespace, path);
+	}
+}
